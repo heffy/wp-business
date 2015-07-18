189e25f9cf07877eb6ad8ed786e0beb58bdc9498
Tony Edwards <tony@plymouthsoftware.com>
2015-07-18 11:27:18 +0100

    Adds plugin info
    Prevents clashing with existing plugin from the WordPress repository.
    Prevents 'update available' message

--- wp-business.php
+++ wp-business.php
@@ -1,13 +1,18 @@
  <?php
+/*
+Plugin Name: Purely Penzance Custom Business Post
+Plugin URI: https://github.com/heffy/wp-business
+Description: A custome porst type for recording business details
+Author: Jacques Heffernan
+Version: 1.0
+Author URI: http://www.jacquesheffernan.co.uk
+*/
 
- /*
-   Plugin Name: PP - Business Post
- */
-
- //Exit if accessed directly
+//Exit if accessed directly
 if( ! defined('ABSPATH')){
 	exit;
 }
 
 require ( plugin_dir_path(__FILE__). 'wp-business-cpt.php');
 require ( plugin_dir_path(__FILE__). 'wp-business-fields.php');
+?>
